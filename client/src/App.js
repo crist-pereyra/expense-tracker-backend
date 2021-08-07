bb7ce318b10d2a1691c40ff71e2a264723e74b76
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2021-08-07 12:24:48 -0400

    Create Backend

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Header } from './comps/Header';
+import { Balance } from './comps/Balance';
+import { IncomeExpenses } from './comps/IncomeExpenses';
+import {TransactionList } from './comps/TransactionList';
+import { AddTransaction } from './comps/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
